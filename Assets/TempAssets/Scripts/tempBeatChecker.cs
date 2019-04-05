7ffe7a2f40d139e5d2dd89d5a6b9e577e063e1cf
Phantomen <robin007@live.se>
2019-04-05 11:15:06 +0200

    Added a beater
    Added a test beater and some objects for testing

---
+++ Assets/TempAssets/Scripts/tempBeatChecker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempBeatChecker : MonoBehaviour {
+
+    //https://answers.unity.com/questions/821530/call-function-on-all-objects-in-trigger.html
+
+
+    [SerializeField]
+    private float perfectDistance = 0.1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (gameObject.GetComponent<Collider>().)
+        }
+        */
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Mathf.Abs(transform.position.x - other.transform.position.x) <= perfectDistance)
+            {
+                Debug.Log("Perfect");
+            }
+
+            else
+            {
+                Debug.Log("Beat hit");
+            }
+
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/TempAssets/Scripts/tempSpawnerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempSpawnerScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject beatObjectPrefab;
+
+    [SerializeField]
+    private float bpm = 20;
+
+    private float currentTime = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= 60/bpm
+            && beatObjectPrefab)
+        {
+            currentTime -= 60 / bpm;
+            GameObject beat = Instantiate(beatObjectPrefab, transform.position, transform.rotation);
+            Destroy(beat, 10.0f);
+        }
+	}
+}

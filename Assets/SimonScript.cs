b4417bf43cd7cc42a458b601d9272bd19023a1d5
Unknown <joelttyjoel@hotmail.com>
2019-04-16 15:27:33 +0200

    Added images for beats, added music

---
+++ Assets/SimonScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+
+public class SimonScript : MonoBehaviour {
+
+    public StudioEventEmitter myEmitter;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.S))
+        {
+            myEmitter.EventInstance.triggerCue();
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            myEmitter.SetParameter("Restart", 1f);
+            
+        }
+	}
+}

9af7c585db080348ad6536fadbcfafbf9aca4580
Ciao Liuang <xiao_liang0605@hotmail.com>
2019-04-17 10:10:07 +0200

    Late changes sowwi

--- Assets/TempAssets/TestingPauseMeny/InputManager.cs
+++ Assets/TempAssets/TestingPauseMeny/InputManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class InputManager : MonoBehaviour {
 
@@ -31,6 +32,11 @@ public class InputManager : MonoBehaviour {
 			return false;
 		}
 		return Input.GetKeyDown (buttonKeys [buttonName]);
+	}
+
+	public string[] GetButtonNames()
+	{
+		return buttonKeys.Keys.ToArray ();
 	}
 
 }

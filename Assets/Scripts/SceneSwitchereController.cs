4b8e28e6308e481827b4a471cd979f4fd19b8738
Unknown <joelttyjoel@hotmail.com>
2019-04-26 10:31:07 +0200

    Added basic tiddy

---
+++ Assets/Scripts/SceneSwitchereController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class SceneSwitchereController : MonoBehaviour {
+    //List of all scenes
+    public List<Object> all_Scenes;
+    //List of all sequencs for battle scene
+    public List<Info_Sequence> all_Sequences_Sp;
+    public List<Info_Sequence> all_Sequences_Mp;
+    //Namme current scene
+    public string nameCurrentScene;
+    //Name current sequence in battle
+    public string nameCurrentSequence;
+    //Single or multiplayer
+    public bool isSp = true;
+
+
+    public static SceneSwitchereController instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        //now replaces already existing gameManager instead
+        else if (instance != this)
+            Destroy(instance.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadSceneByName(string nameOfScene, string nameOfSequence)
+    {
+        //load scene no matter what
+        EditorSceneManager.LoadSceneAsync(nameOfScene);
+        nameCurrentScene = nameOfScene;
+        //Set name of current Sequence so other objects can read from correct one on launch
+        if (nameOfSequence != "") nameCurrentSequence = nameOfSequence; 
+    }
+    
+    void Start () {
+        
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ScriptableObjects/Info_Sequence.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New_Info_Sequence", menuName = "New_Info_Sequences", order = 51)]
+public class Info_Sequence : ScriptableObject
+{
+    //For GameManager
+    [SerializeField]
+    public string[] beatMapNamesInOrder;
+    [SerializeField]
+    public float beatsSpawnToGoal_akaSpeed = 2f;
+
+    //For MusicController
+    [SerializeField]
+    public string pathToFmodEvent = "event:/Funk/FunkSong1";
+}

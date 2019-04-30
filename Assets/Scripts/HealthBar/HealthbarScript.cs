8a8bf972b2576cf175b782975e019b6ae0593b15
Phantomen <robin007@live.se>
2019-04-30 10:46:51 +0200

    Sorted files

---
+++ Assets/Scripts/HealthBar/HealthbarScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthbarScript : MonoBehaviour {
+	
+	public float dmg;
+
+	private Vector3 initialScale;
+	public bool takingDamage;
+	private bool stopCoroutine;
+
+	void Start()
+	{
+		takingDamage = false;
+	}
+
+	void Update()
+	{
+		if (takingDamage) 
+		{
+			takingDamage = false;
+			stopCoroutine = true;
+			StartCoroutine(lerpScale (dmg));
+		}
+
+		if (Input.GetKeyDown (KeyCode.P)) 
+		{
+			
+			takingDamage = true;
+		}
+	}
+
+	IEnumerator lerpScale(float damage)
+	{
+		initialScale = transform.localScale;
+
+		Vector3 targetScale = initialScale - new Vector3 (damage / 100f, 0f, 0f);
+		float time = 1f - (initialScale.x - 1f);
+		float originalTime = time;
+		stopCoroutine = false;
+
+		while(time > 0f)
+		{
+			time -= Time.deltaTime;
+			originalTime += Time.deltaTime * 2.5f;
+			if (stopCoroutine) 
+			{
+				targetScale = transform.localScale;
+				break;
+			}
+
+			if (transform.localScale.x > 0) 
+			{
+				transform.localScale = Vector3.Lerp (targetScale, initialScale, time / originalTime);
+			}
+			if(transform.localScale.x < 0)
+			{
+				transform.localScale = new Vector3 (0, transform.localScale.y, transform.localScale.z);
+			}
+
+			yield return 0;
+
+		}
+		if(transform.localScale.x < 0)
+		{
+			targetScale = new Vector3 (0, transform.localScale.y, transform.localScale.z);
+		}
+		transform.localScale = targetScale;
+	}
+
+
+
+}
---
+++ Assets/Scripts/Inputs/KeyRemapper.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.EventSystems;
+
+public class KeyRemapper : MonoBehaviour {
+
+	Dictionary<string,Text> buttonToLabel;
+
+	InputManager inputManager;
+	public GameObject KeyItemPrefab;
+	public GameObject keyList;
+
+	string buttonToRebind = null;
+	private bool afterClick = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		inputManager = GameObject.FindObjectOfType<InputManager> ();
+		string[] buttonNames = inputManager.GetButtonNames ();
+		buttonToLabel = new Dictionary<string, Text> ();
+
+	
+		foreach (string bn in buttonNames) 
+		{
+			GameObject go = (GameObject)Instantiate (KeyItemPrefab);
+			go.transform.SetParent (keyList.transform);
+
+			Text buttonNameText = go.transform.Find ("ButtonNames").GetComponent<Text>();
+			buttonNameText.text = bn;
+
+
+			Text keyNameText = go.transform.Find ("Button/Keyname").GetComponent<Text>();
+			keyNameText.text = inputManager.GetKeyNameForButton(bn);
+			buttonToLabel [bn] = keyNameText;
+
+			Button keybindButton = go.transform.Find ("Button").GetComponent<Button> ();
+			keybindButton.onClick.AddListener ( () => { StartRebindFor(bn); } );
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (buttonToRebind != null) 
+		{
+			EventSystem.current.sendNavigationEvents = false;
+			if (Input.anyKeyDown && afterClick) 
+			{
+				foreach (KeyCode kc in Enum.GetValues (typeof(KeyCode))) 
+				{
+					if (Input.GetKeyDown (kc)) 
+					{
+						inputManager.SetButtonForKey (buttonToRebind, kc);
+						buttonToLabel [buttonToRebind].text = kc.ToString ();
+						buttonToRebind = null;
+						StartCoroutine (ActivateNavigationEvent (0.5f));
+						break;
+					}
+
+				}
+			}
+			afterClick = true;
+		}
+	}
+
+	private IEnumerator ActivateNavigationEvent(float waitTime)
+	{
+		while (waitTime > 0) 
+		{
+			waitTime -= Time.deltaTime;
+			yield return 0;
+		}
+		EventSystem.current.sendNavigationEvents = true;
+	}
+
+	void StartRebindFor(string buttonName)
+	{		
+		afterClick = false;
+		Debug.Log (buttonName);
+		buttonToRebind = buttonName;
+	}
+
+}

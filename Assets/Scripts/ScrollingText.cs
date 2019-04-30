d428fcbcf55488d6abf5826ad6717da6b89a9d48
Ciao Liuang <xiao_liang0605@hotmail.com>
2019-04-30 17:25:54 +0200

    ADDED Starwars INtro

---
+++ Assets/Scripts/ScrollingText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingText : MonoBehaviour {
+
+	private bool mover;
+	public GameObject introSign;
+	// Use this for initialization
+	void Start () {
+		mover = true;
+	}
+
+	IEnumerator TextScroller()
+	{
+		float timer = 30f;
+		while(timer > 0)
+		{
+			timer -= Time.deltaTime;
+			Vector3 pos = this.gameObject.transform.position;
+			pos.z += Time.deltaTime * 4f;
+			pos.y += Time.deltaTime;
+			this.gameObject.transform.position = pos;
+			Quaternion target = Quaternion.Euler(0, 0, 0);
+			transform.rotation = Quaternion.Slerp(transform.rotation, target,  Time.deltaTime * 0.01f);
+
+
+			yield return 0;
+		}
+		Instantiate (introSign);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mover) 
+		{
+			mover = false;
+
+			StartCoroutine(TextScroller());
+		}
+			
+	}
+}

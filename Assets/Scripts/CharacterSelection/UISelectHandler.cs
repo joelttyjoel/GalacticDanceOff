6c252e26fd9341b44c38a13bd91c5527c919ec21
Xiao Liang <xiao_liang0605@hotmail.com>
2019-05-13 16:34:16 +0200

    error fix

--- Assets/Scripts/CharacterSelection/UISelectHandler.cs
+++ Assets/Scripts/CharacterSelection/UISelectHandler.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
 public class UISelectHandler : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	public void OnSelect(BaseEventData eventData)
+	{
+		Debug.Log (this.gameObject.name + " was selected");
+	}
+
 	// Update is called once per frame
 	void Update () {
 		

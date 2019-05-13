90757cfa38b0e9d3082172678755f00e5c373ff3
Phantomen <robin007@live.se>
2019-05-13 14:02:46 +0200

    Fireworks test scene

---
+++ Assets/Scripts/Effects/FireworksScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireworksScript : MonoBehaviour {
+
+    [SerializeField]
+    private float startSpeed = 5f;
+
+    [SerializeField]
+    private float lifetime = 3f;
+    private float currentLifeTime = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Rigidbody rigBod = transform.GetComponent<Rigidbody>();
+        rigBod.velocity.Set(0 , startSpeed, 0);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentLifeTime += Time.deltaTime;
+
+        if (currentLifeTime >= lifetime)
+        {
+            Destroy(gameObject);
+        }
+	}
+}

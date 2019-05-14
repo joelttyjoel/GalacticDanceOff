7a6530623a9078b9f2f04b2bd305e75bfcbbd268
Xiao Liang <xiao_liang0605@hotmail.com>
2019-05-14 11:45:02 +0200

    fixed shits

--- Assets/Scripts/HealthBar/Health.cs
+++ Assets/Scripts/HealthBar/Health.cs
@@ -25,11 +25,11 @@ public class Health : MonoBehaviour
 	void Update()
 	{
 		if (gameObject.CompareTag ("Player 1")) {
-			currentHealth = GameManagerController.instance.playerHealth;
+			currentHealth = Mathf.Clamp(GameManagerController.instance.playerHealth, 0f, GameManagerController.instance.maxHealth);
 		}
 		if (gameObject.CompareTag ("Player 2")) 
 		{
-			currentHealth = GameManagerController.instance.AIHealth;
+			currentHealth = Mathf.Clamp (GameManagerController.instance.playerHealth, 0f, GameManagerController.instance.maxHealth);
 		}
 
 		//handleBar (currentHealth);
@@ -42,25 +42,7 @@ public class Health : MonoBehaviour
 		if (healthBar.fillAmount != currentHealth / maxHealth) 
 		{
 			healthBar.fillAmount = Mathf.Lerp (healthBar.fillAmount, currentHealth/maxHealth, Time.deltaTime*3f);
-		}
-	}
 
-
-	//makes EGOBar always move towards its current EGO
-	private void handleBar(float currentHealth)
-	{
-		if (transform.localScale.y != currentHealth / maxHealth) 
-		{
-			Vector3 targetVect = new Vector3 (transform.localScale.x, 
-				Mathf.Clamp(currentHealth / maxHealth, 0, maxHealth/maxHealth), transform.localScale.z);
-	
-			transform.localScale = Vector3.Lerp (transform.localScale, 
-					targetVect, Time.deltaTime* 3f);
-			if (transform.localScale.y <= 0.01f) 
-			{
-				targetVect.y = 0;
-				transform.localScale = targetVect;
-			}
 		}
 	}
 }

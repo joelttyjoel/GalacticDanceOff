983ed1678b35d80e8e9934f2c401d6a4e386587c
Xiao Liang <xiao_liang0605@hotmail.com>
2019-05-15 16:59:23 +0200

    Added Textures and TOTAL FIXED ALL MENUS, TRY BUG ME

--- Assets/Scripts/StartMenu/ChangeSpriteIfInteracted.cs
+++ Assets/Scripts/StartMenu/ChangeSpriteIfInteracted.cs
@@ -10,10 +10,10 @@ public class ChangeSpriteIfInteracted : MonoBehaviour {
 	public Sprite changeSprite1;
 	public Sprite changeSprite2;
 	private Vector3 originalScale;
-	private float resize;
+	public float resize;
 	// Use this for initialization
 	void Start () {
-		resize = 1.3f;
+		//resize = 1.3f;
 		image = GetComponent<Image> ();
 		originalScale = this.transform.localScale;
 	}
@@ -27,15 +27,16 @@ public class ChangeSpriteIfInteracted : MonoBehaviour {
                 image.sprite = changeSprite2;
 				this.transform.localScale = originalScale * resize;
 			}
-		} else 
-		{
-			if (image.sprite != changeSprite1) 
-			{
-                Debug.Log("Switch sound PART 2");
-
-                image.sprite = changeSprite1;
-				this.transform.localScale = originalScale;
-			}
 		}
+		else if (image.sprite != changeSprite1 ) 
+		{
+			Debug.Log("Switch sound PART 2");
+
+			image.sprite = changeSprite1;
+			this.transform.localScale = originalScale;
+		}
+
+
+
 	}
 }

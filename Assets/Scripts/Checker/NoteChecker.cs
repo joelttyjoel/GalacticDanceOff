adea32ae2814b29361d788e1585eee69280ce935
Unknown <joelttyjoel@hotmail.com>
2019-05-17 14:03:12 +0200

    Before Friday Meeting

--- Assets/Scripts/Checker/NoteChecker.cs
+++ Assets/Scripts/Checker/NoteChecker.cs
@@ -373,7 +373,7 @@ public class NoteChecker : MonoBehaviour {
         Debug.Log("Hit");
         StartCoroutine(FadeOnHitImage(goodPerfMissSprites[0]));
         GameManagerController.instance.addScore(true, false);
-        AudioController.instance.PlayNoteSound(1f);
+        //AudioController.instance.PlayNoteSound(1f);
     }
 
     //Perfect timed hit
@@ -383,7 +383,7 @@ public class NoteChecker : MonoBehaviour {
         Debug.Log("Perfect");
         StartCoroutine(FadeOnHitImage(goodPerfMissSprites[1]));
         GameManagerController.instance.addScore(true, true);
-        AudioController.instance.PlayNoteSound(2f);
+        //AudioController.instance.PlayNoteSound(2f);
     }
 
     private IEnumerator FadeOnHitImage(SpriteRenderer spriteToDo)

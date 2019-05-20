21ed8255ce751f85aa37fd2fadc0163fbf838106
Xiao Liang <xiao_liang0605@hotmail.com>
2019-05-20 16:08:44 +0200

    Fixed rotation Char select

--- Assets/Scripts/CharacterSelection/UISelectHandler.cs
+++ Assets/Scripts/CharacterSelection/UISelectHandler.cs
@@ -9,10 +9,33 @@ public class UISelectHandler : MonoBehaviour, ISelectHandler {
 	private Vector3 targetRotation;
 	public Animator stageAnimator;
 
+	private Vector3 targetDir;
+
+	Dictionary <int, Vector3> CharSelect;
+	int currentSelect = 1;
+	float speed = 3f;
+
+	void OnEnable()
+	{
+		CharSelect = new Dictionary<int, Vector3> ();
+		CharSelect [0] = new Vector3 (35, -109, -67); //birb
+		CharSelect [1] = new Vector3(-78, -169, -83); //square
+		CharSelect [2] = new Vector3(-14, -114, 12); //napkin
+	}
+
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		targetRotation = this.transform.localScale;
 		stageAnimator = GetComponent<Animator> ();
+		Invoke ("DisableAnimator", 2.5f);
+		targetDir = CharSelect [currentSelect] - transform.position;
+	}
+
+	private void DisableAnimator()
+	{
+		stageAnimator.enabled = false;
 	}
 
 	public void OnSelect(BaseEventData eventData)
@@ -21,14 +44,40 @@ public class UISelectHandler : MonoBehaviour, ISelectHandler {
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		if (Input.GetKeyDown (KeyCode.Return)) 
 		{
 			Debug.Log (EventSystem.current.currentSelectedGameObject);
 		}
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) 
 		{
+			currentSelect += 1;
+			if (currentSelect == 3)
+				currentSelect = 0;
 			
+			targetDir = CharSelect [currentSelect] - transform.position;
+
+		} 
+		else if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			currentSelect -= 1;
+			if (currentSelect == -1)
+				currentSelect = 2;
+
+			targetDir = CharSelect [currentSelect] - transform.position;
+
 		}
+			
+		Quaternion newDir = Quaternion.LookRotation (new Vector3(targetDir.x, 0, -targetDir.z));
+		transform.rotation = Quaternion.Slerp (transform.rotation, newDir, speed * Time.deltaTime);
+
+
 	}
+
+
+			
+
+
+
 }

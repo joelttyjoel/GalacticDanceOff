d27ca548243cfc898d92b8cc087ee9355ae1853a
Xiao Liang <xiao_liang0605@hotmail.com>
2019-05-22 15:09:02 +0200

    Fixed Inputs for XBOX in start Menu and charselect

---
+++ Assets/Scripts/SkipIntroButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SkipIntroButton : MonoBehaviour {
+
+	public Button skipButton;
+
+	// Use this for initialization
+	void Start () {
+		skipButton = GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Return)) 
+		{
+			skipButton.onClick.Invoke ();
+		}
+	}
+}

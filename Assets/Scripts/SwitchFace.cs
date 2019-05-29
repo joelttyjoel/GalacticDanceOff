8e17e046a4ee9432b61805fd156d1454f9f2d095
Robin Sollfors <robin007@live.se>
2019-05-29 14:56:01 +0200

    The faces change
    Changing faces is now working

---
+++ Assets/Scripts/SwitchFace.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchFace : MonoBehaviour {
+
+    [SerializeField]
+    private Material[] faceMaterials = new Material[1];
+
+    private Animator anim;
+
+    private SkinnedMeshRenderer faceRenderer;
+
+    private int faceInt;
+
+    Material[] mat;
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponentInParent<Animator>();
+
+        faceRenderer = GetComponent<SkinnedMeshRenderer>();
+        mat = faceRenderer.materials;
+
+        for (int i = 0; i < mat.Length; i++)
+        {
+            if (mat[i].name.Contains("Face"))
+            {
+                faceInt = i;
+                break;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        int i = anim.GetInteger("SelectState");
+		if (faceMaterials[i])
+        {
+            mat[faceInt] = faceMaterials[i];
+            faceRenderer.materials = mat;
+        }
+	}
+}

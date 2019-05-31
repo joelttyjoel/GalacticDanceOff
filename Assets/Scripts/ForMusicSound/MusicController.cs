0a8a837cea806c99188183a8e60c1961d81b4cbb
Unknown <joelttyjoel@hotmail.com>
2019-05-31 14:33:05 +0200

    Dunno did thing

--- Assets/Scripts/ForMusicSound/MusicController.cs
+++ Assets/Scripts/ForMusicSound/MusicController.cs
@@ -72,7 +72,7 @@ public class MusicController : MonoBehaviour
         float hunnerdPercentage = percentage * 100;
         myEmitter.SetParameter("Score", hunnerdPercentage);
 
-        crowdEmitter.SetParameter("Score", hunnerdPercentage);
+        crowdEmitter.SetParameter("CrowdScore", hunnerdPercentage);
     }
 
     public void PauseMusic()

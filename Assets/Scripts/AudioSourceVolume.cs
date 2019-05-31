2e698f855aca6e8e4efa77f63003fd7f34aca577
Robin Sollfors <robin007@live.se>
2019-05-31 15:07:08 +0200

    Announcer volume
    Announcer volume tied to sound volume

---
+++ Assets/Scripts/AudioSourceVolume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceVolume : MonoBehaviour {
+
+    private SceneSwitchereController ssc;
+    private AudioSource audSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ssc = SceneSwitchereController.instance;
+        audSource = transform.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        audSource.volume = ssc.volumeSound;
+	}
+}

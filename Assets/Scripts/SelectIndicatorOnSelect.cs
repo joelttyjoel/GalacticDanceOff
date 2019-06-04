c26c7b7bc2e850a98291d3a1d722b21b3a1e906b
Unknown <joelttyjoel@hotmail.com>
2019-06-04 10:36:48 +0200

    wokring

---
+++ Assets/Scripts/SelectIndicatorOnSelect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectIndicatorOnSelect : MonoBehaviour {
+
+    public List<Sprite> imagesInOrder;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().sprite = imagesInOrder[SceneSwitchereController.instance.selectedCharacter];
+	}
+}

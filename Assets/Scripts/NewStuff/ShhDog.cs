f207137b6f350d27070c137c76297aa6e7dd8279
Unknown <joelttyjoel@hotmail.com>
2019-09-24 10:29:44 +0200

    Changes before adding music

---
+++ Assets/Scripts/NewStuff/ShhDog.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShhDog : MonoBehaviour {
+    
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.J))
+        {
+            GetComponent<SpriteRenderer>().enabled = !GetComponent<SpriteRenderer>().enabled;
+        }
+	}
+}
